8f7c9bf79d50683b82b6d8997e246ad573b88650
Aditya singh <107112101+its-me-aditya@users.noreply.github.com>
2023-04-24 22:17:59 +0530

    chef$opt.cpp

---
+++ chef$opt.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int a;
+        cin>>a;
+        int b;
+        cin>>b;
+        if(a<b){
+            cout<<"<"<<endl;
+            
+        }
+        else if (a>b){
+            cout <<">"<<endl;
+        }
+         else if(a==b){
+            cout<<"="<<endl;
+         }
+        
+     
+        
+        
+
+            
+    }
+
+	// your code goes here
+	return 0;
+}
