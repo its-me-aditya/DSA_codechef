e7b3a1c0bb8d41d9c5828276222796da70308a86
Aditya singh <107112101+its-me-aditya@users.noreply.github.com>
2024-02-26 22:01:57 +0530

    Create sclane_tri.cpp

---
+++ sclane_tri.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b,c;
+        cin>>a>>b>>c;
+        if(a!=b && b!=c){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+	// your code goes here
+
+}
